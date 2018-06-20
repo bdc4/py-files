ee1b347a9963edc0f250ccc8ed6c10b5dca2199f
bdc4 <bdc4@stmarys-ca.edu>
2018-06-19 21:18:19 -0700

    removed redundant CrewMembers array

--- mtproto/src/main/main.py
+++ mtproto/src/main/main.py
@@ -1,14 +1,14 @@
 
-#Main
-from tools.dicemachine import RollD
+#System
+import sys, os, copy, pkg_resources
+from os.path import abspath
+from inspect import getsourcefile, getmodule, getsource
+
+#Tools
 import easygui.easygui as gui
 from openpyxl import load_workbook
-import sys
-import os
-import copy
-from inspect import getsourcefile, getmodule, getsource
-from os.path import abspath
-import pkg_resources
+from tools.dicemachine import RollD
+
 
 class Event:
     title = "None"
@@ -78,7 +78,6 @@ class Table:
         
         self.events = copy.copy(events)
 
-
 class Sector:
     name = None
     eventTables = []
@@ -121,8 +120,9 @@ class Sector:
 class Controller:
     day = 0
     sector = None
-    comps = {"Physical":5,"Electrical":5,"Computerized":5}
-    meds = 10
+    inv = {"P":5,"E":5,"C":5,"M":10}
+    mecs = {"M":None,"E":None,"C":None,"S":None}
+    crew = [None]*4
 
     def NextDay(self):
         if self.phase == "END":
@@ -136,36 +136,136 @@ class Controller:
 
     def GetOptions(self):
         #Get possible option choices
-        if self.phase == "START": keys = ["D","S","X"]
+        if self.phase == "START": keys = ["D","S","RC","RP", "X"]
         else: keys = ["E","S","M","X"]
 
         options = [OPTIONS[k] for k in keys]
-
         return options
-    
-class Subsystem:
-    name = None
-    damage = 0
-    sid = None
 
-    def __init__(self, name):
-        self.name = name
-        self.sid = name[:1]
+    def ShipStatus(self):
+        choice = gui.buttonbox("What do you want to check on?","Status Report",["Ship","Crew","Inventory","Done"])
+        while choice != "Done":
+            #This will be our report output
+            textStr = ""
+
+            #Get MECS status and any damaged subsystems
+            if choice == "Ship":
+                for room in self.mecs.items():
+                    room = room[1]
+                    textStr += ("-----\n"+room.name+"\n-----\n")
+                    damStr = ""
+                    for sub in room.subsystems:
+                        if sub.damage != 0:
+                            damStr += (sub.name+" Subsystems:\n"+"    Damage: "+sub.GetSeverity()+"\n")
+                    if damStr == "":
+                        textStr += "Nothing to report...\n"
+                    textStr += "\n"
+            
+            #Get crew status
+            elif choice == "Crew":
+                for crew in self.crew:
+                    textStr += ("\n"+crew.name+"\n-- Room: "+crew.room.name+"\n-- Health: "+crew.GetHealth()+"\n")
+            
+            #Get inventory status
+            elif choice =="Inventory":
+                for name, number in self.inv.items():
+                    textStr += (str(number)+" "+name+" Components\n")
+            
+            #Print report and check for another
+            textStr += "\n\n"
+            choice = gui.buttonbox(textStr+"What else do you want to check on?",choice+" Status",["Ship","Crew","Inventory","Done"])
     
-    def GetSeverity(self):
-        if self.damage == 0:
-            return "No Damage"
-        else:
-            return "Severity Level "+str(self.damage)
+    def InitGame(self):
+
+        def SetCrewNames():
+            msg = "What are your crew members' names?"
+            title = "Crew Names"
+            fieldNames = ["Medbay Specialist","Engines Specialist","Comms Specialist","Systems Specialist"]
+            fieldValues = []  # we start with blanks for the values
+            fieldValues = gui.multenterbox(msg,title, fieldNames)
+
+            # make sure that none of the fields was left blank
+            while True:
+                if fieldValues is None: break
+                errmsg = ""
+                for i in range(len(fieldNames)):
+                    if fieldValues[i].strip() == "":
+                        errmsg += ('"%s" is a required field.\n\n' % fieldNames[i])
+                if errmsg == "":
+                    break # no problems found
+                fieldValues = gui.multenterbox(errmsg, title, fieldNames, fieldValues)
+            return fieldValues
+
+        for label in MECS_LABELS:
+            self.mecs[label[:1]] = MECS(label)
+
+        #init Crew
+        while None in self.crew:
+            qs = gui.ynbox("Quick Start?")
+
+            #Name crew members manually
+            if qs == False:
+
+                #returns array of str names or None if cancelled
+                cnames = SetCrewNames()
+
+                #Don't try to create Crew objects if name population was cancelled
+                if cnames == None: continue
+
+                #Otherwise, create Crew objects from the list of provided names
+                for i in range(0, len(self.crew)):
+                    self.crew[i] = Crew(cnames[i],self.mecs[MECS_LABELS[i][:1]])
+                    
+            #If quickstart is selected, auto-name crew members
+            if qs == True:
+                for i in range(0, len(self.crew)):
+                    self.crew[i] = Crew("Jenkins "+str(i+1),self.mecs[MECS_LABELS[i][:1]])
+
+    def MoveCrew(self):
+        crewNames = []
+        for crew in self.crew:
+            crewNames.append(crew.name)
         
+        while True:
+            who = gui.buttonbox("Who would you like to move?","Pick Crew",crewNames)
+            
+            for crew in self.crew:
+                if who == crew.name:
+                    crew.ChangeRoom()
+                    return
+            
+            print("Not a valid selection! Try again...")
+    
+    def GetRandomCrew(self):
+        return self.crew[RollD(4)-1]
+
+    def GetRandomPECs(self):
+        roll = RollD(3)
+        key = PECS_LABELS[roll-1]
+        return key, self.inv[key[:1]]
+
 class MECS:
+
+    class Subsystem:
+        name = None
+        damage = 0
+        sid = None
+
+        def __init__(self, name):
+            self.name = name
+            self.sid = name[:1]
+        
+        def GetSeverity(self):
+            if self.damage == 0:
+                return "No Damage"
+            else:
+                return "Severity Level "+str(self.damage)
+
     name = None
     subsystems = [Subsystem("Physical"),Subsystem("Electrical"),Subsystem("Computerized")]
 
     def __init__(self, name):
         self.name = name
-
-
 
 class Crew:
     name = None
@@ -193,7 +293,8 @@ class Crew:
     def ChangeRoom(self):
         while True:
             where = gui.buttonbox("Where should "+self.name+" be moved?\n","Room Reassignment",MECS_LABELS)
-            for room in ROOMS:
+            for room in GC.mecs.items():
+                room = room[1]
                 if where == room.name:
                     oldRoom = self.room.name
                     self.room = room
@@ -201,142 +302,36 @@ class Crew:
                     return
             print("Room not found. Try again...")
 
-def ShipStatus():
-    choice = ""
-    while choice != "Done":
-        
-        choice = gui.buttonbox("What do you want to check on?","Ship Status",["Ship","Crew","Inventory","Done"])
-        
-        if choice == "Ship":
-            textStr = ""
-            for room in ROOMS:
-                textStr += ("-----\n"+room.name+"\n-----\n")
-                damStr = ""
-                for sub in room.subsystems:
-                    if sub.damage != 0:
-                        damStr += (sub.name+" Subsystems:\n"+"    Damage: "+sub.GetSeverity()+"\n")
-                if damStr == "":
-                    textStr += "Nothing to report...\n"
-                textStr += "\n"
-            choice = gui.buttonbox(textStr,"Ship Status",["Back","Done"])
-        
-        elif choice == "Crew":
-            textStr = ""
-            #textStr += ("\n-----\nCrew Status\n-----\n")
-            for crew in CrewMembers:
-                textStr += ("\n"+crew.name+"\n-- Room: "+crew.room.name+"\n-- Health: "+crew.GetHealth()+"\n")
-            choice = gui.buttonbox(textStr,"Crew Status",["Back","Done"])
-        
-        elif choice =="Inventory":
-            textStr = ""
-            #textStr += ("\n-----\nComponents\n-----\n")
-            for comp in PECS:
-                #msgbox(comp)
-                textStr += (str(GC.comps[comp])+" "+comp+" Components\n")
-            textStr += str(GC.meds)+" Medical Supplies"
-            choice = gui.buttonbox(textStr,"Crew Status",["Back","Done"])
-                
-def MoveCrew():
-    while True:
-        crewNames = []
-        for crew in CrewMembers:
-            crewNames.append(crew.name)
-        who = gui.buttonbox("Who would you like to move?","Pick Crew",crewNames)
-        
-        for crew in CrewMembers:
-            if who == crew.name:
-                crew.ChangeRoom()
-                return
-        
-        print("Not a valid selection! Try again...")
-
-def GetRandomCrew():
-    return CrewMembers[RollD(4)-1]
-
-def GetRandomPECs():
-    roll = RollD(3)
-    return PECS[roll-1]
-
-def SetCrewNames():
-    msg = "What are your crew members' names?"
-    title = "Crew Names"
-    fieldNames = ["Medbay Specialist","Engines Specialist","Comms Specialist","Systems Specialist"]
-    fieldValues = []  # we start with blanks for the values
-    fieldValues = gui.multenterbox(msg,title, fieldNames)
-
-    # make sure that none of the fields was left blank
-    while True:
-        if fieldValues is None: break
-        errmsg = ""
-        for i in range(len(fieldNames)):
-            if fieldValues[i].strip() == "":
-                errmsg += ('"%s" is a required field.\n\n' % fieldNames[i])
-        if errmsg == "":
-            break # no problems found
-        fieldValues = gui.multenterbox(errmsg, title, fieldNames, fieldValues)
-    return fieldValues
-
 #####Globals
 
 #define global arrays
 MECS_LABELS = ["Medbay","Engines","Comms","Systems"]
-EVENT_TYPES = ["NOTHING","EVENTS","MEETINGS","OTHER"]
-PECS = ["Physical","Electrical","Computerized"]
+PECS_LABELS = ["Physical","Electrical","Computerized"]
+test = MECS("test").Subsystem("test")
+print(test)
+#All possible menu options
+OPTIONS = dict(E="Next Day", D="Start Day",S="Status Report",M="Move Crew Member",RC="Get Random Crew",RP="Get Random PECs",X="Exit Program")
 
 #init Sectors
 Sector1 = Sector()
 Sector1.name = "Alpha Sector"
 Sector1.eventTables = [Table("NOTHING",1,50),Table("EVENTS",51,71),Table("MEETINGS",72,92),Table("OTHER",93,100)]
 Sector1.setDataFile("data/DailyEventsTable.xlsx")
-#init Ship
-ROOMS = [None]*4
-CrewMembers = [None]*4
 
 #init Game Controller (GC)
 GC = Controller()
 GC.sector = Sector1
+GC.phase = "START"
 
 #####End Globals
 
-#Offer Quickstart
-
-#init Rooms
-for i in range(0, len(MECS_LABELS)):
-    ROOMS[i] = MECS(MECS_LABELS[i])
-
-#init Crew
-while None in CrewMembers:
-    qs = gui.ynbox("Quick Start?")
-
-    #Name crew members manually
-    if qs == False:
-
-        #returns array of str names or None if cancelled
-        cnames = SetCrewNames()
-
-        #Don't try to create Crew objects if name population was cancelled
-        if cnames == None: continue
-
-        #Otherwise, create Crew objects from the list of provided names
-        for i in range(0, len(ROOMS)):
-            CrewMembers[i] = Crew(cnames[i],ROOMS[i])
-            
-    #If quickstart is selected, auto-name crew members
-    if qs == True:
-        for i in range(0, len(ROOMS)):
-            CrewMembers[i] = Crew("Jenkins "+str(i+1),ROOMS[i])
-
-#####
-            
-#All possible menu options
-OPTIONS = dict(E="Next Day", D="Start Day",S="Status Report",M="Move Crew Member",RC="Get Random Crew",
-               RP="Get Random PECs",X="Exit Program")
 
 def __main__():
 
+    GC.InitGame()
+
     action = ""
-    GC.phase = "START"
-    
+
     while action != "exit":
 
         #Return option list depending on phase
@@ -353,13 +348,13 @@ def __main__():
         if action == OPTIONS["D"] or action == OPTIONS["E"]:
             GC.NextDay()
         elif action == OPTIONS["S"]:
-            ShipStatus()
+            GC.ShipStatus()
         elif action == OPTIONS["M"]:
-            MoveCrew()
+            GC.MoveCrew()
         elif action == OPTIONS["RC"]:
-            gui.msgbox(GetRandomCrew().name)
+            gui.msgbox(GC.GetRandomCrew().name)
         elif action == OPTIONS["RP"]:
-            gui.msgbox(GetRandomPECs())
+            gui.msgbox(GC.GetRandomPECs())
         elif action == OPTIONS["X"]:
             if gui.ynbox("Are you sure you want to quit?","Quit Program?"):
                 sys.exit()
